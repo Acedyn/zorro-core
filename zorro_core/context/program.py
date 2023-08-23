dcdc87f7ae272cdff7bcbb5eafac8f66bbe07680
Lambin Simon <simon.lamb98@gmail.com>
2023-08-23 20:41:44 +0200

    unit: add the start client unit test

---
+++ zorro_core/context/program.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+
+
+class Program(BaseModel):
+    """
+    A program defines an installed program an how to
+    launch it
+    """
+
+    name: str
+    tags: list[str] = Field(default_factory=list)
+    subsets: list[str] = Field(default_factory=list)
+    launch_template: list[str] = Field(default_factory=lambda: ["{name}"])
+    launch_client_template: list[str] = Field(default_factory=lambda: ["{name}"])
--- zorro_core/schedulers/client.py
+++ zorro_core/schedulers/client.py
@@ -1,41 +1,12 @@
 import asyncio
 from asyncio.subprocess import Process
-from pathlib import Path
 from enum import IntEnum
 from typing import Optional
 
 from pydantic import BaseModel, ConfigDict, Field
 
 from zorro_core.context.context import Context
-
-
-class Program(BaseModel):
-    """
-    A program defines an installed program an how to
-    launch it
-    """
-
-    name: str
-    path: Path = Field(default=Path())
-    launch_client_template: list[str] = Field(default_factory=lambda: ["{name}"])
-
-    async def start_as_client(self, context: Context, metadata: Optional[dict] = None):
-        """
-        Start the program and return a client as a handle for it
-        """
-
-        metadata = metadata or {}
-        client = Client(program=self, context=context, metadata=metadata)
-
-        command = [
-            key.format({**metadata, "name": self.name, "path": self.path})
-            for key in self.launch_client_template
-        ]
-        process = await asyncio.create_subprocess_exec(*command, env=context.build_environment())
-
-        client.process = process
-        client.pid = process.pid
-        return client
+from zorro_core.context.program import Program
 
 
 class ClientStatus(IntEnum):
@@ -53,6 +24,7 @@ class Client(BaseModel):
     A client is bound to a running program, with a client running to
     receive and process commands
     """
+
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     program: Program
@@ -61,3 +33,26 @@ class Client(BaseModel):
     process: Optional[Process] = Field(default=None, exclude=True)
     metadata: dict = Field(default_factory=dict)
     status: ClientStatus = Field(default=ClientStatus.INITIALIZED)
+
+    @classmethod
+    async def start_program(
+        cls, program: Program, context: Context, metadata: Optional[dict] = None
+    ):
+        """
+        Start the program and return a client as a handle for it
+        """
+
+        metadata = metadata or {}
+        client = cls(program=program, context=context, metadata=metadata)
+
+        command = [
+            key.format(**{**metadata, "name": program.name})
+            for key in program.launch_client_template
+        ]
+        process = await asyncio.create_subprocess_exec(
+            *command, env=context.build_environment()
+        )
+
+        client.process = process
+        client.pid = process.pid
+        return client
