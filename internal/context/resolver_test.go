989d02aeb0a6901282edb8dbf60853071077c015
Simon Lambin <simon@lestontonstruqueurs.com>
2023-09-12 08:46:30 +0200

    feat: plugin resolution from search paths

--- internal/context/resolver_test.go
+++ internal/context/resolver_test.go
@@ -8,9 +8,9 @@ var testVersionComparisons = map[struct {
 	a string
 	b string
 }]VersionOperator{
-	{a: "3.5", b: "3.4"}: VersionOperator_MORE_EQUAL,
-	{a: "2.1.4", b: "2.1.4"}: VersionOperator_EQUAL,
-	{a: "2.1", b: "2.1.4"}: VersionOperator_EQUAL,
+	{a: "3.5", b: "3.4"}:            VersionOperator_MORE_EQUAL,
+	{a: "2.1.4", b: "2.1.4"}:        VersionOperator_EQUAL,
+	{a: "2.1", b: "2.1.4"}:          VersionOperator_EQUAL,
 	{a: "3.5.alpha", b: "3.5.beta"}: VersionOperator_LESS_EQUAL,
 	{a: "1.5.prod", b: "3.5.alpha"}: VersionOperator_LESS_EQUAL,
 }
@@ -19,7 +19,7 @@ func TestVersionComparison(t *testing.T) {
 	for comparison, expectedResult := range testVersionComparisons {
 		result := CompareVersions(comparison.a, comparison.b)
 		if result != expectedResult {
-      t.Errorf("Invalid version comparison result: %s %s %s (expected: %s)", comparison.a, result, comparison.b, expectedResult)
+			t.Errorf("Invalid version comparison result: %s %s %s (expected: %s)", comparison.a, result, comparison.b, expectedResult)
 		}
 	}
 }
