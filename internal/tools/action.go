882abfb5679f72df02ca2b870c2d92425509b225
Simon Lambin <simon@lestontonstruqueurs.com>
2023-09-16 15:35:59 +0200

    unit: add action traversal tests

--- internal/tools/action.go
+++ internal/tools/action.go
@@ -7,9 +7,15 @@ import (
 	"github.com/life4/genesis/slices"
 )
 
+// Hold the returned value of a child task
+type ChildTaskResult struct {
+  Err error
+  Key string
+}
+
 // Find and traverse children that have all their dependencies (upstream)
 // completed.
-func (action *Action) getReadyChildren(pending, completed map[string]bool) map[string]TraversableTool {
+func (action *Action) getReadyChildren(pending map[string]bool, completed []string) map[string]TraversableTool {
 	readyChildren := map[string]TraversableTool{}
 	for childKey, child := range action.GetChildren() {
 		// Skip already process children
@@ -19,15 +25,16 @@ func (action *Action) getReadyChildren(pending, completed map[string]bool) map[s
 
 		// Process children that don't have dependencies or that have
 		// completed dependencies
-		if child.Upstream == nil || completed[child.GetUpstream()] {
+		if child.Upstream == nil || slices.All(child.Upstream, func(el string) bool {return slices.Contains(completed, el)}) {
 			switch child.GetChild().(type) {
 			case *ActionChild_Action:
 				readyChildren[childKey] = child.GetAction()
+			case *ActionChild_Command:
+				readyChildren[childKey] = child.GetCommand()
 			}
 		}
 	}
 	return readyChildren
-
 }
 
 // Run the task to all the children, respecting the order of execution
@@ -40,8 +47,8 @@ func (action *Action) Traverse(task func(TraversableTool) error) error {
 
 	// At first, all the children are pending
 	pending := maps.FromKeys(maps.Keys(action.GetChildren()), true)
-	completed := map[string]bool{}
-	tasksResults := make(chan error, 1)
+	completed := []string{}
+	tasksResults := make(chan *ChildTaskResult, 1)
 	errors := []error{}
 	// Hack to make sure the for loop executes at least once
 	tasksResults <- nil
@@ -49,7 +56,8 @@ func (action *Action) Traverse(task func(TraversableTool) error) error {
 	// Wait for the next completed task unil the channel is closed
 	for taskResult := range tasksResults {
 		if taskResult != nil {
-			errors = append(errors, taskResult)
+			completed = append(completed, taskResult.Key)
+			errors = append(errors, taskResult.Err)
 		}
 
 		readyChildren := action.getReadyChildren(pending, completed)
@@ -57,14 +65,16 @@ func (action *Action) Traverse(task func(TraversableTool) error) error {
 			// All the ready children are executed in their own goroutine
 			pending[childKey] = false
 			go func(childKey string, child TraversableTool) {
-				tasksResults <- task(child)
-				completed[childKey] = true
+				tasksResults <- &ChildTaskResult{
+          Err: child.Traverse(task),
+          Key: childKey,
+        }
 			}(childKey, child)
 		}
 
 		// If there is as many completed children as non pending, then the
 		// action done
-		if len(completed) == len(slices.Filter(maps.Values(pending), func(el bool) bool { return el })) {
+		if len(completed) == len(slices.Filter(maps.Values(pending), func(el bool) bool { return !el })) {
 			close(tasksResults)
 		}
 	}
---
+++ internal/tools/action_test.go
@@ -0,0 +1,275 @@
+package tools
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/life4/genesis/maps"
+	"github.com/life4/genesis/slices"
+)
+
+// Test the resolution of the children that are ready to be traversed
+type GetReadyChildrenTest struct {
+	Action             *Action
+	Pending            map[string]bool
+  Completed           []string
+	ExpectedReady      []string
+}
+
+var getReadyChildrenTests = []GetReadyChildrenTest{
+	{
+		Action: &Action{
+			Children: map[string]*ActionChild{
+				"1": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+				},
+				"1-1": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+					Upstream: []string{"1"},
+				},
+				"2": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+				},
+				"2-1": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+					Upstream: []string{"2"},
+				},
+			},
+		},
+		Pending: map[string]bool{
+			"1":   false,
+			"1-1": true,
+			"2":   false,
+			"2-1": true,
+		},
+		Completed: []string{"1"},
+		ExpectedReady: []string{"1-1"},
+	},
+	{
+		Action: &Action{
+			Children: map[string]*ActionChild{
+				"1": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+				},
+				"2": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+				},
+				"3": {
+					Child: &ActionChild_Command{
+						Command: &Command{},
+					},
+					Upstream: []string{"1", "2"},
+				},
+			},
+		},
+		Pending: map[string]bool{
+			"1":   false,
+			"2": false,
+			"3":   true,
+		},
+		Completed: []string{"1"},
+		ExpectedReady: []string{},
+	},
+}
+
+func TestGetReadyChildren(t *testing.T) {
+	for _, getReadyChildrenTest := range getReadyChildrenTests {
+		readyChildren := getReadyChildrenTest.Action.getReadyChildren(
+			getReadyChildrenTest.Pending,
+			getReadyChildrenTest.Completed,
+		)
+		if !slices.Equal(maps.Keys(readyChildren), getReadyChildrenTest.ExpectedReady) {
+			t.Errorf("Incorrect ready children set resolved (received %s, expected %s)",
+				maps.Keys(readyChildren),
+				getReadyChildrenTest.ExpectedReady,
+			)
+		}
+	}
+}
+
+// Test the action traversal order
+var actionTraversalTests = []*Action{
+	{
+		Base: &ToolBase{
+			Name: &[]string{"0"}[0],
+		},
+		Children: map[string]*ActionChild{
+			"00-A": {
+				Child: &ActionChild_Action{
+					Action: &Action{
+						Base: &ToolBase{
+							Name: &[]string{"00-A"}[0],
+						},
+						Children: map[string]*ActionChild{
+							"000-A": {
+								Child: &ActionChild_Command{
+									Command: &Command{
+										Base: &ToolBase{
+											Name: &[]string{"000-A"}[0],
+										},
+									},
+								},
+							},
+							"001-A": {
+								Child: &ActionChild_Command{
+									Command: &Command{
+										Base: &ToolBase{
+											Name: &[]string{"001-A"}[0],
+										},
+									},
+								},
+								Upstream: []string{"000-A"},
+							},
+							"002-A": {
+								Child: &ActionChild_Command{
+									Command: &Command{
+										Base: &ToolBase{
+											Name: &[]string{"002-A"}[0],
+										},
+									},
+								},
+								Upstream: []string{"001-A"},
+							},
+							"002-B": {
+								Child: &ActionChild_Action{
+									Action: &Action{
+										Base: &ToolBase{
+											Name: &[]string{"002-B"}[0],
+										},
+										Children: map[string]*ActionChild{
+											"0020-A": {
+												Child: &ActionChild_Command{
+													Command: &Command{
+														Base: &ToolBase{
+															Name: &[]string{"0020-A"}[0],
+														},
+													},
+												},
+											},
+											"0020-B": {
+												Child: &ActionChild_Command{
+													Command: &Command{
+														Base: &ToolBase{
+															Name: &[]string{"0020-B"}[0],
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+								Upstream: []string{"001-A"},
+							},
+							"002-C": {
+								Child: &ActionChild_Command{
+									Command: &Command{
+										Base: &ToolBase{
+											Name: &[]string{"002-C"}[0],
+										},
+									},
+								},
+								Upstream: []string{"001-A"},
+							},
+						},
+					},
+				},
+			},
+			"01-A": {
+				Child: &ActionChild_Action{
+					Action: &Action{
+						Base: &ToolBase{
+							Name: &[]string{"01-A"}[0],
+						},
+						Children: map[string]*ActionChild{
+							"010-A": {
+								Child: &ActionChild_Command{
+									Command: &Command{
+										Base: &ToolBase{
+											Name: &[]string{"010-A"}[0],
+										},
+									},
+								},
+							},
+							"011-A": {
+								Child: &ActionChild_Command{
+									Command: &Command{
+										Base: &ToolBase{
+											Name: &[]string{"011-A"}[0],
+										},
+									},
+								},
+								Upstream: []string{"010-A"},
+							},
+						},
+					},
+				},
+				Upstream: []string{"00-A"},
+			},
+			"01-B": {
+				Child: &ActionChild_Command{
+					Command: &Command{
+						Base: &ToolBase{
+							Name: &[]string{"01-B"}[0],
+						},
+					},
+				},
+				Upstream: []string{"00-A"},
+			},
+			"02-A": {
+				Child: &ActionChild_Command{
+					Command: &Command{
+						Base: &ToolBase{
+							Name: &[]string{"02-A"}[0],
+						},
+					},
+				},
+				Upstream: []string{"01-A", "01-B"},
+			},
+		},
+	},
+}
+
+func TestActionTraversal(t *testing.T) {
+	traversalHistory := []string{}
+	for _, actionTraversalTest := range actionTraversalTests {
+		actionTraversalTest.Traverse(func(tool TraversableTool) error {
+			traversalHistory = append(traversalHistory, tool.GetBase().GetName())
+			return nil
+		})
+
+		if len(traversalHistory) < len(actionTraversalTest.GetChildren()) {
+			t.Errorf(
+				"Action traversal did not traversed the full tree (%d children traversed)",
+				len(traversalHistory),
+			)
+		}
+		for index, traversedKey := range traversalHistory {
+			if index == 0 {
+				continue
+			}
+      traversedKey = strings.Split(traversedKey, "-")[0]
+      previousTraversedKey := strings.Split(traversalHistory[index-1], "-")[0]
+      minLenght, _ := slices.Min([]int{len(previousTraversedKey), len(traversedKey)})
+
+      if strings.Compare(previousTraversedKey[:minLenght], traversedKey[:minLenght]) == 1 {
+				t.Errorf(
+					"Action traversal ran in incorrect order (%s < %s)",
+					previousTraversedKey,
+					traversedKey,
+				)
+			}
+		}
+	}
+}
