298e2024c5c23adbed6695955288abde12d4390f
Simon Lambin <simon@lestontonstruqueurs.com>
2023-09-16 16:11:18 +0200

    feat: handle multiple upstreams

--- internal/tools/action_test.go
+++ internal/tools/action_test.go
@@ -10,10 +10,10 @@ import (
 
 // Test the resolution of the children that are ready to be traversed
 type GetReadyChildrenTest struct {
-	Action             *Action
-	Pending            map[string]bool
-  Completed           []string
-	ExpectedReady      []string
+	Action        *Action
+	Pending       map[string]bool
+	Completed     []string
+	ExpectedReady []string
 }
 
 var getReadyChildrenTests = []GetReadyChildrenTest{
@@ -50,7 +50,7 @@ var getReadyChildrenTests = []GetReadyChildrenTest{
 			"2":   false,
 			"2-1": true,
 		},
-		Completed: []string{"1"},
+		Completed:     []string{"1"},
 		ExpectedReady: []string{"1-1"},
 	},
 	{
@@ -75,11 +75,11 @@ var getReadyChildrenTests = []GetReadyChildrenTest{
 			},
 		},
 		Pending: map[string]bool{
-			"1":   false,
+			"1": false,
 			"2": false,
-			"3":   true,
+			"3": true,
 		},
-		Completed: []string{"1"},
+		Completed:     []string{"1"},
 		ExpectedReady: []string{},
 	},
 }
@@ -100,176 +100,172 @@ func TestGetReadyChildren(t *testing.T) {
 }
 
 // Test the action traversal order
-var actionTraversalTests = []*Action{
-	{
-		Base: &ToolBase{
-			Name: &[]string{"0"}[0],
+var actionTraversalTest = Action{
+	Base: &ToolBase{
+		Name: &[]string{"0"}[0],
+	},
+	Children: map[string]*ActionChild{
+		"00-A": {
+			Child: &ActionChild_Action{
+				Action: &Action{
+					Base: &ToolBase{
+						Name: &[]string{"00-A"}[0],
+					},
+					Children: map[string]*ActionChild{
+						"000-A": {
+							Child: &ActionChild_Command{
+								Command: &Command{
+									Base: &ToolBase{
+										Name: &[]string{"000-A"}[0],
+									},
+								},
+							},
+						},
+						"001-A": {
+							Child: &ActionChild_Command{
+								Command: &Command{
+									Base: &ToolBase{
+										Name: &[]string{"001-A"}[0],
+									},
+								},
+							},
+							Upstream: []string{"000-A"},
+						},
+						"002-A": {
+							Child: &ActionChild_Command{
+								Command: &Command{
+									Base: &ToolBase{
+										Name: &[]string{"002-A"}[0],
+									},
+								},
+							},
+							Upstream: []string{"001-A"},
+						},
+						"002-B": {
+							Child: &ActionChild_Action{
+								Action: &Action{
+									Base: &ToolBase{
+										Name: &[]string{"002-B"}[0],
+									},
+									Children: map[string]*ActionChild{
+										"0020-A": {
+											Child: &ActionChild_Command{
+												Command: &Command{
+													Base: &ToolBase{
+														Name: &[]string{"0020-A"}[0],
+													},
+												},
+											},
+										},
+										"0020-B": {
+											Child: &ActionChild_Command{
+												Command: &Command{
+													Base: &ToolBase{
+														Name: &[]string{"0020-B"}[0],
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+							Upstream: []string{"001-A"},
+						},
+						"002-C": {
+							Child: &ActionChild_Command{
+								Command: &Command{
+									Base: &ToolBase{
+										Name: &[]string{"002-C"}[0],
+									},
+								},
+							},
+							Upstream: []string{"001-A"},
+						},
+					},
+				},
+			},
 		},
-		Children: map[string]*ActionChild{
-			"00-A": {
-				Child: &ActionChild_Action{
-					Action: &Action{
-						Base: &ToolBase{
-							Name: &[]string{"00-A"}[0],
+		"01-A": {
+			Child: &ActionChild_Action{
+				Action: &Action{
+					Base: &ToolBase{
+						Name: &[]string{"01-A"}[0],
+					},
+					Children: map[string]*ActionChild{
+						"010-A": {
+							Child: &ActionChild_Command{
+								Command: &Command{
+									Base: &ToolBase{
+										Name: &[]string{"010-A"}[0],
+									},
+								},
+							},
 						},
-						Children: map[string]*ActionChild{
-							"000-A": {
-								Child: &ActionChild_Command{
-									Command: &Command{
-										Base: &ToolBase{
-											Name: &[]string{"000-A"}[0],
-										},
+						"011-A": {
+							Child: &ActionChild_Command{
+								Command: &Command{
+									Base: &ToolBase{
+										Name: &[]string{"011-A"}[0],
 									},
 								},
 							},
-							"001-A": {
-								Child: &ActionChild_Command{
-									Command: &Command{
-										Base: &ToolBase{
-											Name: &[]string{"001-A"}[0],
-										},
-									},
-								},
-								Upstream: []string{"000-A"},
-							},
-							"002-A": {
-								Child: &ActionChild_Command{
-									Command: &Command{
-										Base: &ToolBase{
-											Name: &[]string{"002-A"}[0],
-										},
-									},
-								},
-								Upstream: []string{"001-A"},
-							},
-							"002-B": {
-								Child: &ActionChild_Action{
-									Action: &Action{
-										Base: &ToolBase{
-											Name: &[]string{"002-B"}[0],
-										},
-										Children: map[string]*ActionChild{
-											"0020-A": {
-												Child: &ActionChild_Command{
-													Command: &Command{
-														Base: &ToolBase{
-															Name: &[]string{"0020-A"}[0],
-														},
-													},
-												},
-											},
-											"0020-B": {
-												Child: &ActionChild_Command{
-													Command: &Command{
-														Base: &ToolBase{
-															Name: &[]string{"0020-B"}[0],
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-								Upstream: []string{"001-A"},
-							},
-							"002-C": {
-								Child: &ActionChild_Command{
-									Command: &Command{
-										Base: &ToolBase{
-											Name: &[]string{"002-C"}[0],
-										},
-									},
-								},
-								Upstream: []string{"001-A"},
-							},
+							Upstream: []string{"010-A"},
 						},
 					},
 				},
 			},
-			"01-A": {
-				Child: &ActionChild_Action{
-					Action: &Action{
-						Base: &ToolBase{
-							Name: &[]string{"01-A"}[0],
-						},
-						Children: map[string]*ActionChild{
-							"010-A": {
-								Child: &ActionChild_Command{
-									Command: &Command{
-										Base: &ToolBase{
-											Name: &[]string{"010-A"}[0],
-										},
-									},
-								},
-							},
-							"011-A": {
-								Child: &ActionChild_Command{
-									Command: &Command{
-										Base: &ToolBase{
-											Name: &[]string{"011-A"}[0],
-										},
-									},
-								},
-								Upstream: []string{"010-A"},
-							},
-						},
+			Upstream: []string{"00-A"},
+		},
+		"01-B": {
+			Child: &ActionChild_Command{
+				Command: &Command{
+					Base: &ToolBase{
+						Name: &[]string{"01-B"}[0],
 					},
 				},
-				Upstream: []string{"00-A"},
 			},
-			"01-B": {
-				Child: &ActionChild_Command{
-					Command: &Command{
-						Base: &ToolBase{
-							Name: &[]string{"01-B"}[0],
-						},
+			Upstream: []string{"00-A"},
+		},
+		"02-A": {
+			Child: &ActionChild_Command{
+				Command: &Command{
+					Base: &ToolBase{
+						Name: &[]string{"02-A"}[0],
 					},
 				},
-				Upstream: []string{"00-A"},
 			},
-			"02-A": {
-				Child: &ActionChild_Command{
-					Command: &Command{
-						Base: &ToolBase{
-							Name: &[]string{"02-A"}[0],
-						},
-					},
-				},
-				Upstream: []string{"01-A", "01-B"},
-			},
+			Upstream: []string{"01-A", "01-B"},
 		},
 	},
 }
 
 func TestActionTraversal(t *testing.T) {
 	traversalHistory := []string{}
-	for _, actionTraversalTest := range actionTraversalTests {
-		actionTraversalTest.Traverse(func(tool TraversableTool) error {
-			traversalHistory = append(traversalHistory, tool.GetBase().GetName())
-			return nil
-		})
+	actionTraversalTest.Traverse(func(tool TraversableTool) error {
+		traversalHistory = append(traversalHistory, tool.GetBase().GetName())
+		return nil
+	})
 
-		if len(traversalHistory) < len(actionTraversalTest.GetChildren()) {
-			t.Errorf(
-				"Action traversal did not traversed the full tree (%d children traversed)",
-				len(traversalHistory),
-			)
+	if len(traversalHistory) < 14 {
+		t.Errorf(
+			"Action traversal did not traversed the full tree (%d children traversed)",
+			len(traversalHistory),
+		)
+	}
+	for index, traversedKey := range traversalHistory {
+		if index == 0 {
+			continue
 		}
-		for index, traversedKey := range traversalHistory {
-			if index == 0 {
-				continue
-			}
-      traversedKey = strings.Split(traversedKey, "-")[0]
-      previousTraversedKey := strings.Split(traversalHistory[index-1], "-")[0]
-      minLenght, _ := slices.Min([]int{len(previousTraversedKey), len(traversedKey)})
+		traversedKey = strings.Split(traversedKey, "-")[0]
+		previousTraversedKey := strings.Split(traversalHistory[index-1], "-")[0]
+		minLenght, _ := slices.Min([]int{len(previousTraversedKey), len(traversedKey)})
 
-      if strings.Compare(previousTraversedKey[:minLenght], traversedKey[:minLenght]) == 1 {
-				t.Errorf(
-					"Action traversal ran in incorrect order (%s < %s)",
-					previousTraversedKey,
-					traversedKey,
-				)
-			}
+		if strings.Compare(previousTraversedKey[:minLenght], traversedKey[:minLenght]) == 1 {
+			t.Errorf(
+				"Action traversal ran in incorrect order (%s < %s)",
+				previousTraversedKey,
+				traversedKey,
+			)
 		}
 	}
 }
