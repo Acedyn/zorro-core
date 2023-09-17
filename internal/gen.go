588494929b72a1e4933dddf4565a86424d2c2c54
Lambin Simon <simon.lamb98@gmail.com>
2023-09-17 11:18:40 +0200

    cicd: exclude the generate proto from git

--- internal/gen.go
+++ internal/gen.go
@@ -1,6 +1,8 @@
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./context/context.proto
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./context/plugin.proto
 
+//go:generate protoc --go_out=. --go_opt=paths=source_relative ./config/config.proto
+
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./tools/tool.proto
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./tools/action.proto
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./tools/command.proto
