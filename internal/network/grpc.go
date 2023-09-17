2670f0fa533979d0444bca5fadf7979db990ab8b
Simon Lambin <simon@lestontonstruqueurs.com>
2023-09-17 11:55:04 +0200

    feat: create the grpc server

---
+++ internal/network/grpc.go
@@ -0,0 +1,35 @@
+package network
+
+import (
+	"fmt"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcServer *grpc.Server
+	once   sync.Once
+)
+
+// Getter for the grpc server singleton
+func GrpcServer() *grpc.Server {
+	once.Do(func() {
+		grpcServer = grpc.NewServer()
+	})
+
+	return grpcServer
+}
+
+// Start the grpc server
+func ServeGrpc(host string, port int) error {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		return fmt.Errorf("failed to listen: %w", err)
+	}
+  if err = GrpcServer().Serve(listener); err != nil {
+		return fmt.Errorf("failed to server grpc: %w", err)
+  }
+  return nil
+}
