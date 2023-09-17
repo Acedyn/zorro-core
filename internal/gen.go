33815041c28b16dab5dcabd8a375de9749844222
Lambin Simon <simon.lamb98@gmail.com>
2023-09-17 11:43:23 +0200

    feat: create the scheduler proto file

--- internal/gen.go
+++ internal/gen.go
@@ -8,4 +8,5 @@
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./tools/command.proto
 
 //go:generate protoc --go_out=. --go_opt=paths=source_relative ./scheduling/client.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative ./scheduling/scheduler.proto
 package internal
