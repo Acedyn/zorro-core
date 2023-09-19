722b472f3caa686b17a51a221843054dd7073bd6
Lambin Simon <simon.lamb98@gmail.com>
2023-09-19 14:49:04 +0200

    feat: handle updates with clients over grpc

--- internal/scheduling/scheduler.go
+++ internal/scheduling/scheduler.go
@@ -1,48 +1,140 @@
 package scheduling
 
 import (
-	"context"
+	"fmt"
+	"io"
 
 	"github.com/Acedyn/zorro-core/internal/network"
-	"github.com/Acedyn/zorro-core/internal/client"
+	"github.com/Acedyn/zorro-core/internal/tools"
 )
 
 type schedulingServer struct {
-  UnimplementedSchedulingServer
+	UnimplementedSchedulingServer
 }
 
-func (server *schedulingServer) RegisterClient(c context.Context, client *client.Client) (*client.Client, error) {
-  // clientPoolLock.Lock()
-  // defer clientPoolLock.Unlock()
-  // clientHandle, ok := ClientPool()[int(client.GetPid())]
-  // if !ok {
-  //   return nil, fmt.Errorf("could not register client %s: client not found in client pool", client)
-  // }
+// Handle update received from the client
+func receiveUpdate(registedClient *RegisteredClient, stream Scheduling_CommunicateCommandsServer, stop chan error) error {
+	// Loop until we shoud stop listen for updates
+	for range stop {
+		in, err := stream.Recv()
+		// The communication is closed
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return fmt.Errorf("an error occured while receiving message: %w", err)
+		}
+		response := &ClientCommunication{}
 
-  // // Patch the local version of the client
-  // maps.Update(clientHandle.Client.Metadata, client.GetMetadata())
-  // if client.Label != nil {
-  //   clientHandle.Client.Label = client.Label
-  // }
-  // if client.Status != nil {
-  //   clientHandle.Client.Status = client.Status
-  // }
+		// Handle client update
+		if in.ClientUpdate != nil {
+			if registedClient.Client.Patch(in.ClientUpdate) {
+				response.ClientUpdate = registedClient.Client
+			}
+		}
 
-  // // Send the patched version
-  // return clientHandle.Client, nil
-  return nil, nil
+		// Handle command updates
+		if in.CommandUpdate != nil {
+			registedClient.ScheduledCommandsLock.Lock()
+			command, ok := registedClient.ScheduledCommands[in.CommandUpdate.Base.GetId()]
+			registedClient.ScheduledCommandsLock.Unlock()
+
+			if !ok {
+				return fmt.Errorf(
+					"no command with id %s exists under client %s",
+					in.CommandUpdate.Base.GetId(),
+					registedClient.Client.GetId(),
+				)
+			}
+
+			if command.Patch(in.CommandUpdate) {
+				response.CommandUpdate = command
+			}
+		}
+
+		err = stream.Send(response)
+		if err != nil {
+			return fmt.Errorf("an error occured while sending message: %w", err)
+		}
+	}
+
+	return nil
 }
 
-func (server *schedulingServer) CommunicateCommands(stream Scheduling_CommunicateCommandsServer) error {
-  return nil
+// Send update to the connected client
+func sendUpdates(registedClient *RegisteredClient, stream Scheduling_CommunicateCommandsServer, stop chan error) error {
+	// Loop until we shoud stop sending updates
+	for range stop {
+		select {
+		// Send the new command queries
+		case command := <-registedClient.CommandQueue:
+			// Send the command to the client
+			err := stream.Send(&ClientCommunication{CommandUpdate: command})
+			if err != nil {
+				return fmt.Errorf("an error occured while sending message: %w", err)
+			}
+
+			// Store the client in the running command list
+			registedClient.ScheduledCommandsLock.Lock()
+			registedClient.ScheduledCommands[command.Base.GetId()] = command
+			registedClient.ScheduledCommandsLock.Unlock()
+		case <-stop:
+			break
+		}
+	}
+
+	return nil
 }
 
+// Exchange updates about the running commands, context with clients
+func (server *schedulingServer) Communicate(stream Scheduling_CommunicateCommandsServer) error {
+	// Start with registering the client
+	in, err := stream.Recv()
+	if err == io.EOF {
+		return nil
+	} else if err != nil {
+		return fmt.Errorf("an error occured while receiving message: %w", err)
+	} else if in.ClientUpdate == nil {
+		return fmt.Errorf("no client update received: all client communications should start with client registration")
+	}
+	registeredClient := registerClient(in.ClientUpdate)
+
+	// Send an receive updates
+	error := make(chan error)
+	go func() {
+		error <- receiveUpdate(registeredClient, stream, error)
+	}()
+	go func() {
+		error <- sendUpdates(registeredClient, stream, error)
+	}()
+
+	// Wait for any goroutine to exit
+	err = <-error
+	// Make sure to stop both the coroutines
+	close(error)
+	return err
+}
+
+// Wait for command requests and dispach then to the corresponding registered client
 func listenCommandRequests() {
-  // for command := range tools.CommandPool() {
-    // client := ClientFromQuery(command.Command.GetClientQuery())
-  // }
+	for commandHandle := range tools.CommandQueue() {
+		registeredClient, err := ClientFromQuery(commandHandle.Context, commandHandle.Command.GetClientQuery())
+		if err != nil {
+			// The error should be handled by the command submitter
+			commandHandle.Result <- fmt.Errorf(
+				"could not get or create client from query %s in the selected context: %w",
+				commandHandle.Command.GetClientQuery(),
+				err,
+			)
+		} else {
+			// The command will then be sent to the client
+			go func(commandHandle *tools.CommandHandle) {
+				registeredClient.CommandQueue <- commandHandle.Command
+			}(commandHandle)
+		}
+	}
 }
 
 func init() {
-  RegisterSchedulingServer(network.GrpcServer(), &schedulingServer{})
+	RegisterSchedulingServer(network.GrpcServer(), &schedulingServer{})
+	go listenCommandRequests()
 }
--- internal/tools/tool.go
+++ internal/tools/tool.go
@@ -1,7 +1,33 @@
 package tools
 
+import (
+	"github.com/life4/genesis/maps"
+)
+
 // Traversable tool are nested tools linked via dependencies
 type TraversableTool interface {
 	Traverse(func(TraversableTool) error) error
 	GetBase() *ToolBase
+}
+
+func (tool *ToolBase) Patch(patch *ToolBase) bool {
+	// Patch the local version of the tool
+	isPatched := false
+
+	if patch.Label != nil && tool.GetLabel() != patch.GetLabel() {
+		tool.Label = patch.Label
+		isPatched = true
+	}
+	if patch.Status != nil && tool.GetStatus() != patch.GetStatus() {
+		tool.Status = patch.Status
+		isPatched = true
+	}
+	if patch.Tooltip != nil && tool.GetTooltip() != patch.GetTooltip() {
+		tool.Tooltip = patch.Tooltip
+		isPatched = true
+	}
+	// Logs are a special case, they are combined together
+	maps.Update(tool.Logs, patch.GetLogs())
+
+	return isPatched
 }
