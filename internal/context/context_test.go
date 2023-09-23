495eb030d151aa87c7e530c86195f42262789369
Simon Lambin <simon@lestontonstruqueurs.com>
2023-09-23 13:01:30 +0200

    unit: write tests for context env

---
+++ internal/context/context_test.go
@@ -0,0 +1,75 @@
+package context
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	context_proto "github.com/Acedyn/zorro-proto/zorroprotos/context"
+	plugin_proto "github.com/Acedyn/zorro-proto/zorroprotos/plugin"
+	"github.com/life4/genesis/slices"
+)
+
+// Mocked context
+var testContext = Context{
+	Context: &context_proto.Context{
+		Plugins: []*plugin_proto.Plugin{
+			{
+				Name: "plugin-a",
+				Env: map[string]*plugin_proto.PluginEnv{
+					"FOO": {
+						Append: []string{
+							"/plugin-a/a",
+							"/plugin-a/b",
+						},
+					},
+					"BAR": {
+						Prepend: []string{
+							"/plugin-a/a",
+							"/plugin-a/b",
+						},
+					},
+				},
+			},
+			{
+				Name: "plugin-b",
+				Env: map[string]*plugin_proto.PluginEnv{
+					"FOO": {
+						Prepend: []string{
+							"/plugin-b/a",
+							"/plugin-b/b",
+						},
+					},
+					"BAZ": {
+						Set: &[]string{"plugin-b"}[0],
+					},
+				},
+			},
+		},
+	},
+}
+
+// Test the resolution of a environment of a context
+func TestEnviron(t *testing.T) {
+	resolvedEnviron := testContext.Environ(false)
+	expectedEnviron := map[string]string{
+    "FOO": strings.Join([]string{
+			"/plugin-b/b",
+			"/plugin-b/a",
+			"/plugin-a/a",
+			"/plugin-a/b",
+		}, string(filepath.ListSeparator)),
+    "BAR": strings.Join([]string{
+			"/plugin-a/b",
+			"/plugin-a/a",
+		}, string(filepath.ListSeparator)),
+    "BAZ": "plugin-b",
+	}
+	fmt.Println(resolvedEnviron)
+	for key, environ := range expectedEnviron {
+		if !slices.Contains(resolvedEnviron, key + "=" + environ) {
+			t.Errorf("No resolved environ matched the expected environ %q", environ)
+		}
+	}
+}
